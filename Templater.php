99f603e1742883de6953282a1f31225b0b4fc769
Alexandr Drozd <alexandr1drozd@gmail.com>
2015-11-28 21:27:12 +0200

    Update Templater.php

--- Templater.php
+++ Templater.php
@@ -11,24 +11,29 @@
 class Templater {
 	
 	var $dir = '.',
+	    $type = '.tpl',
 	    $temp = array(),
 	    $get = array();
+		
+	function __construct($dir, $type){
+		if($dir) $this->dir = $dir;
+		if($type) $this->type = $type;
+	}
 	
 	public function set($tpl, $data, $blocks = false, $res = 'main', $cache = true){
 		
-		if($tpl OR file_exists($this->dir.$tpl.'.tpl')){
-			
+		if($tpl OR file_exists($this->dir.$tpl.$this->type)){
 			if(!$cache OR !$this->get[$res])
-				$this->temp[$res] = file_get_contents($this->dir.$tpl.'.tpl');
+				$this->temp[$res] = file_get_contents($this->dir.$tpl.$this->type);
 			
 			$temp = str_replace(array_keys($data), array_values($data), $this->temp[$res]);
-
-	            	if (is_array($blocks)){
-	                    foreach ($blocks as $key => $value) {
-	                        $temp = preg_replace("#\\[".$key."\\](.*?)\\[/".$key."\\]#is", $value ? "$1" : "", $temp);
-	                    }
-	                }
-	                
+			
+			if (is_array($blocks)){
+				foreach ($blocks as $key => $value) {
+					$temp = preg_replace("#\\[".$key."\\](.*?)\\[/".$key."\\]#is", $value ? "$1" : "", $temp);
+				}
+			}
+			
 			if(isset($this->get[$res]) and $this->get[$res])
 				$this->get[$res] .= $temp;
 			else
@@ -37,12 +42,12 @@ class Templater {
 			return $temp;
 			
 		} else
-			die("No template file: ".$this->dir.$tpl.'.tpl');
+			die("No template file: ".$this->dir.$tpl.$this->type);
 	}
 
 	function clear() {
-		$this->get = [];
-		$this->data = [];
+		$this->get = array();
+		$this->data = array();
 	}
 }
 ?>
